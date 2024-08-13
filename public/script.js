869eaf32fbee434e125167bb74a72b8cdbda34f6
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-13 15:28:08 +0530

    refactor

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-import * as api from './api';
+import * as api from './api.js';
 
 document.getElementById('login-btn').addEventListener(async event => {
     window.location.href = await api.getLoginUrl().authUrl;

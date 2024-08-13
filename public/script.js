8915d04aaa9b75597bba0203c74d6981c5bc165b
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-13 16:19:04 +0530

    Fixed undefind login url

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 import * as api from './api.js';
 
 document.getElementById('login-btn').addEventListener('click', async () => {
-    window.location.href = await api.getLoginUrl().url;
+    let loginUrl = await api.getLoginUrl();
+    window.location.href = loginUrl.url;
 })

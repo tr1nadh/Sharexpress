5459e1ccbe4c0a6225da832c4454c4b22547b172
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-13 15:04:23 +0530

    initial commit

---
+++ api/auth/callback.js
@@ -0,0 +1,23 @@
+import {OAuth2Client} from 'google-auth-library'; 
+
+const CLIENT_ID = '747178828000-rd118ek1tcsuuk0tfa84np1f0thtb2he.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+
+const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+const SCOPES = ['https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/userinfo.profile'
+];
+
+export const callback = async (req, res) => {
+    const code = req.query.code;
+    try {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        res.send('true');
+    } catch (error) {
+        console.error(error);
+        res.send('false');
+    }
+};
---
+++ api/auth/login.js
@@ -0,0 +1,19 @@
+import {OAuth2Client} from 'google-auth-library'; 
+
+const CLIENT_ID = '747178828000-rd118ek1tcsuuk0tfa84np1f0thtb2he.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+
+const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+const SCOPES = ['https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/userinfo.profile'
+];
+
+export const auth = (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    })
+    res.send(authUrl);
+};

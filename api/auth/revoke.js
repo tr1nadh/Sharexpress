9ada2d569313e18b97fe769ab23136a5044a66a5
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-19 17:47:16 +0530

    revoke fix 3

--- api/auth/revoke.js
+++ api/auth/revoke.js
@@ -3,10 +3,10 @@ import {oAuth2Client} from '../../utils/g-oAuth-client';
 
 export default async function(req, res) {
     const cred = getCredFromCookies(req);
+    clearCredCookies(res);
     try {
         await oAuth2Client.revokeToken(cred.refresh_token);
         console.log('Token revoked successfully');
-        clearCredCookies(res);
         res.send({url: process.env.DOMAIN});
     } catch (error) {
         console.error('Error revoking token:', error);

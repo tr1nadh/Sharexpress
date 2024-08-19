ba390971cfbe091a691c0303e2b4af8bdf1cc670
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-19 19:32:48 +0530

    Added user email as a scope

--- api/auth/url.js
+++ api/auth/url.js
@@ -2,7 +2,8 @@ import {oAuth2Client} from '../../utils/g-oAuth-client';
 
 const SCOPES = ['https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/userinfo.profile'
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
 ];
 
 export default function(req, res) {

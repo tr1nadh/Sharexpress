ce0b4e199f7901cc230737fc1115530f00734fa0
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-19 19:32:30 +0530

    Added user id as email

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,11 +1,15 @@
 import {oAuth2Client} from '../../utils/g-oAuth-client';
+import {google} from 'googleapis';
 import {createOrUpdateUser} from '../../utils/user-cookie-manager';
 
 export default async function(req, res) {
     const code = req.query.code;
     try {
         const { tokens } = await oAuth2Client.getToken(code);
-        createOrUpdateUser(res, tokens);
+        oAuth2Client.setCredentials(tokens);
+        const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
+        const { data } = await oauth2.userinfo.get();
+        createOrUpdateUser(data.email, res, tokens);
         res.redirect('/files');
     } catch (error) {
         res.status(500).json({error: 'Internal server error'});

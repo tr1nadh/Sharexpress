397f909de86260ef7463070f9d6a34d5515e4b5d
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-19 17:28:25 +0530

    Fixed revoking user not properly happening

--- api/auth/revoke.js
+++ api/auth/revoke.js
@@ -1,20 +1,15 @@
 import {getCredFromCookies, clearCredCookies} from '../../utils/user-cookie-manager';
+import {oAuth2Client} from '../../utils/g-oAuth-client';
 
 export default async function(req, res) {
     const cred = getCredFromCookies(req);
-    const url = `https://oauth2.googleapis.com/revoke?token=${cred.refresh_token}`;
     try {
-        const response = await fetch(url, { method: 'POST' });
-        if (response.ok) {
-            console.log('Token revoked successfully');
-        } else {
-            console.error('Failed to revoke token:', response.statusText);
-        }
+        await oAuth2Client.revokeToken(cred.refresh_token);
+        console.log('Token revoked successfully');
     } catch (error) {
         console.error('Error revoking token:', error);
     }
 
     clearCredCookies(res);
     res.send({url: process.env.DOMAIN});
-    console.log(process.env.DOMAIN);
 }

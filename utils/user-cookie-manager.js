e147992db62748c289f5f1d34a56db1c416fd589
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-08-19 19:33:14 +0530

    Added id to manage

--- utils/user-cookie-manager.js
+++ utils/user-cookie-manager.js
@@ -1,11 +1,12 @@
 import {getCookieExpireTime} from './date-and-time-utils';
 import cookie from 'cookie';
 
-export function createOrUpdateUser(res, tokens) {
+export function createOrUpdateUser(id, res, tokens) {
   const cookieExpireTime = getCookieExpireTime(tokens.expiry_date);
   const maxAge_30Days = (30 * 24 * 60 * 60 * 1000);
   const isSecure = process.env.NODE_ENV === 'prod' ? 'Secure;' : '';
   res.setHeader('Set-Cookie', [
+    `id=${id}; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=${maxAge_30Days}`,
     `access_token=${tokens.access_token}; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=${cookieExpireTime}`,
     `refresh_token=${tokens.refresh_token}; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=${maxAge_30Days}`,
     `expiry_date=${tokens.expiry_date}; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=${maxAge_30Days}`,
@@ -15,6 +16,7 @@ export function createOrUpdateUser(res, tokens) {
 export function clearCredCookies(res) {
   const isSecure = process.env.NODE_ENV === 'prod' ? 'Secure;' : '';
   res.setHeader('Set-Cookie', [
+      `id=0; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=0`,
       `access_token=0; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=0`,
       `refresh_token=0; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=0`,
       `expiry_date=0; HttpOnly; ${isSecure}; SameSite=Strict; Path=/; Max-Age=0`,
@@ -24,9 +26,10 @@ export function clearCredCookies(res) {
 export function getCredFromCookies(req) {
   const parsedCookies = cookie.parse(req.headers.cookie || '');
   return {
-      access_token: parsedCookies.access_token,
-      refresh_token: parsedCookies.refresh_token,
-      token_type: 'Bearer',
-      expiry_date: parsedCookies.expiry_date
+    id: parsedCookies.id,
+    access_token: parsedCookies.access_token,
+    refresh_token: parsedCookies.refresh_token,
+    token_type: 'Bearer',
+    expiry_date: parsedCookies.expiry_date
   }
 }
